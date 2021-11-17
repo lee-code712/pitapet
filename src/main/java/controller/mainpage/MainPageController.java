ba84af02e27128bc37dc81a1baf53e351dae5075
yuriLee <yurilee07@naver.com>
2021-11-17 21:11:15 +0900

    메인페이지 컨트롤러 수정

--- src/main/java/controller/mainpage/MainPageController.java
+++ src/main/java/controller/mainpage/MainPageController.java
@@ -1,6 +1,8 @@
 package controller.mainpage;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +12,9 @@ import controller.Controller;
 import controller.member.UserSessionUtils;
 import model.dto.Review;
 import model.dto.Care;
+import model.dto.Member;
 import model.service.CareManager;
+import model.service.MemberManager;
 import model.service.ReviewManager;
 
 public class MainPageController implements Controller{
@@ -20,6 +24,7 @@ public class MainPageController implements Controller{
 		HttpSession session = request.getSession();
 		CareManager careMan = CareManager.getInstance();
 		ReviewManager reviewMan = ReviewManager.getInstance();
+		MemberManager memMan = MemberManager.getInstance();
 		
 		if(UserSessionUtils.hasLogined(session)) {
 			 List<Care> careSchedules = careMan.findCareSchedules(UserSessionUtils.getLoginUserId(session));
@@ -31,6 +36,22 @@ public class MainPageController implements Controller{
 		
 		List<Review> reviews = reviewMan.findReviewList();
 		request.setAttribute("reviews", reviews);
+		
+		Random rd = new Random();
+		List<Review> randomReviews = new ArrayList<Review>();
+		for (int i = 0; i < 3; i++) {
+			int randomIndex = rd.nextInt(reviews.size());
+			Review review = reviews.get(randomIndex);
+			Member sitterMemInfo = memMan.findMember(review.getCareInfo().getSitter().getSitter().getId());
+			String[] address = sitterMemInfo.getAddress().split(" ");
+			String city = null;
+			for (int j = 0; j < address.length; j++) {
+				if (address[j].matches("(.*)로"))
+					city = address[j].substring(0, address[j].length() - 2);
+			}
+			review.getCareInfo().getSitter().getSitter().setAddress(city);
+			randomReviews.add(reviews.get(randomIndex));
+		}
 		
 		System.out.print(reviews);
 		
--- src/main/java/model/dao/ReviewDAO.java
+++ src/main/java/model/dao/ReviewDAO.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import model.dto.Review;
 import model.dto.Care;
+import model.dto.Member;
+import model.dto.PetSitter;
 
 public class ReviewDAO {
 
@@ -18,7 +20,8 @@ private JDBCUtil jdbcUtil = null;
 		
 	//전체 리뷰 반환
 	public List<Review> findReviewList() throws SQLException {
-        String sql = "SELECT review_id, write_date, content, rate, care_id FROM review";       
+        String sql = "SELECT review_id, write_date, content, rate, r.care_id, c.member_id, c.sitter_id "
+        		+ "FROM review r JOIN care c USING (care_id)";
 		jdbcUtil.setSqlAndParameters(sql, null);	
 					
 		try {
@@ -30,8 +33,11 @@ private JDBCUtil jdbcUtil = null;
 						rs.getString("write_date"),
 						rs.getString("content"),
 						rs.getFloat("rate"),
-						new Care(rs.getInt("care_id"))
-				);
+						new Care(
+								rs.getInt("care_id"),
+								new Member(rs.getString("member_id")),
+								new PetSitter(
+										new Member(rs.getString("sitter_id")))));
 				reviews.add(review);				
 			}		
 			return reviews;					

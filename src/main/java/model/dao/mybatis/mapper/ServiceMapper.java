6b8f61fe1cf063a77f88ce0a600f4bba71d9e2aa
yuriLee <yurilee07@naver.com>
2021-12-08 21:33:27 +0900

    ServiceMapper 메소드 추가

--- src/main/java/model/dao/mybatis/mapper/ServiceMapper.java
+++ src/main/java/model/dao/mybatis/mapper/ServiceMapper.java
@@ -5,5 +5,6 @@ import java.util.List;
 import model.dto.Service;
 
 public interface ServiceMapper {
+	public int deleteReceiveService(String careId);
 	public List<Service> selectProvideServicesBySitter(String sitterId);
 }
